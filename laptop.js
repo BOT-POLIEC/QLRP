c75de1351b3ada09dcd4b7c347e4175f5dd8ad6c
BOT-POLIEC <138233043+BOT-POLIEC@users.noreply.github.com>
2024-09-24 16:03:31 +0300

    Add files via upload

---
+++ laptop.js
@@ -0,0 +1,8 @@
+const tabs = document.querySelectorAll('.tab');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        document.querySelector('.tab.active').classList.remove('active');
+        tab.classList.add('active');
+    });
+});
